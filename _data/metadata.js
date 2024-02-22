64610f141fe852c14cf5fc453ed99c118684ee75
Dayle Probably <dayle.probably@icloud.com>
2024-02-22 02:18:30 +0000

    update metadata file

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Dayle Probably Blog",
+	url: "https://dayle-probably.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A place for Dayle (probably) to scream into the void.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Dayle Probably",
+		email: "dayle.probably@icloud.com",
+		url: "https://dayle-probably.com/about/"
 	}
 }
